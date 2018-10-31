b22d0cdfa2f6a9927169ff8965167a09689d33e4
alucard263096 <alucard263096@126.com>
2018-10-31 17:42:17 +0800

    commit

--- Source/pages/searchbox/searchbox.js
+++ Source/pages/searchbox/searchbox.js
@@ -1,7 +1,16 @@
 // pages/content/content.js
-import { AppBase } from "../../appbase";
-import { ApiConfig } from "../../apis/apiconfig";
-import { InstApi } from "../../apis/inst.api.js";
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
+import {
+  PhoneApi
+} from "../../apis/phone.api";
 
 class Content extends AppBase {
   constructor() {
@@ -11,19 +20,58 @@ class Content extends AppBase {
     this.Base.Page = this;
     //options.id=5;
     super.onLoad(options);
+    var json = {
+      searchrecomm: "Y"
+    };
+    if (options.new != undefined) {
+      json.newphone = "Y";
+    }
+    var api = new PhoneApi();
+    api.model(json, (result) => {
+      this.Base.setMyData({
+        result
+      });
+    });
   }
   onMyShow() {
     var that = this;
   }
-  bindFocus () {
+  bindFocus() {
     wx.navigateBack({
       delta: -1
     });
-}
+  }
+  search(e) {
+    console.log(e.detail.value);
+
+    var json = {
+
+    };
+    if (e.detail.value == "") {
+      
+      json.searchrecomm = "Y";
+    } else {
+      json.searchkeyword = e.detail.value;
+    }
+
+    if (this.Base.options.new != undefined) {
+      json.newphone = "Y";
+    }
+
+
+    var api = new PhoneApi();
+    api.model(json, (result) => {
+      this.Base.setMyData({
+        result
+      });
+    });
+
+  }
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
 body.bindFocus = content.bindFocus;
+body.search = content.search;
 Page(body)
